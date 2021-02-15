b3540d9da39f01e620f649b5bcb3727f3178fe09
WhiteSicada <dino_villa@hotmail.fr>
2021-02-15 01:21:46 +0100

    first commit

---
+++ src/app/employee.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Employee } from './employee/employee';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EmployeeService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) {}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employees/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employees/add`,employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employees/update`,employee);
+  }
+
+  public deleteEmployee(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employees/delete/${id}`);
+  }
+}
