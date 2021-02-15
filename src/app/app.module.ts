fb60fa089aa788f33d891a2d6080420abdbba9cc
WhiteSicada <dino_villa@hotmail.fr>
2021-02-15 01:40:06 +0100

    Add getEmployees onInit + set apiBaseUrl in env fi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
   ],
   imports: [
     BrowserModule
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Employee } from './employee/employee';
+import { Employee } from './employee';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
-@Injectable({
-  providedIn: 'root',
-})
+@Injectable({providedIn: 'root',})
 export class EmployeeService {
-  private apiServerUrl = '';
+  private apiServerUrl = environment.apiBaseUrl;
 
   constructor(private http: HttpClient) {}
 
